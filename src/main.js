9d714cac0f2af4d124e59a023d10020b2e1109d4
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:07:55 +0530

    internal module,read the file

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
-import { sum } from "./calc.js";
+// How to read a file using NodeJS?
+// this is a internal module.
+import { readFileSync } from "node:fs";
 
-let output = sum(10, 10);
-console.log("sum=" + output);
+let filePath = "C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\day9\package.json";
+let fileData = readFileSync(filePath, { encoding: "utf-8" });
+console.log(fileData);
