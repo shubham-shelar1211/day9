c3d24500b63e1a3b840b369692b42a4682ba04e5
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:29:26 +0530

    read file,async version,promise version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,9 @@
-async function sum(n1, n2) {
-    // ...
-    return n1 + n2;
-  }
-  
-  async function main() {
-    let result = await sum(1, 1);
-    console.log(result);
-  }
-  
-  main();
+import { readFile } from "node:fs/promises";
+
+async function main() {
+  let filePath ="C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\day9\package.json";
+  let fileData = await readFile(filePath, { encoding: "utf-8" });
+  console.log(fileData);
+}
+
+main();
