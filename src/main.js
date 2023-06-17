5f8eb0e84fdc6125a87c31f7da677b2e041b8ad0
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:33:41 +0530

    async-version-to-write-file

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-import { readFile } from "node:fs/promises";
+import { writeFile } from "node:fs/promises";
 
 async function main() {
-  let filePath ="C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\day9\package.json";
-  let fileData = await readFile(filePath, { encoding: "utf-8" });
-  console.log(fileData);
+  let filePath = "C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\output1.txt";
+  let fileData = `Asynchronously writes data to a file, 
+                replacing the file if it already exists. 
+                data can be a string, a buffer, an <AsyncIterable>, 
+                or an <Iterable> object.`;
+  await writeFile(filePath, fileData);
+  console.log("Write Success");
 }
 
 main();
