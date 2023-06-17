ac36629467dd2c9b282766e26f0a662983374157
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:25:22 +0530

    async-and-await

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-async function hello() {
-    console.log("Hello 1");
+async function sum(n1, n2) {
+    // ...
+    return n1 + n2;
   }
   
-  console.log("Hello 1");
-  hello();
-  console.log("Hello 3");
+  async function main() {
+    let result = await sum(1, 1);
+    console.log(result);
+  }
+  
+  main();
