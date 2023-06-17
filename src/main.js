5cf183880b06d0b290858d1f18d53da807ebc5dd
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:37:05 +0530

    exception handling

--- src/main.js
+++ src/main.js
@@ -1,13 +1,13 @@
-import { writeFile } from "node:fs/promises";
+import { readFile } from "node:fs/promises";
 
 async function main() {
-  let filePath = "C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\output1.txt";
-  let fileData = `Asynchronously writes data to a file, 
-                replacing the file if it already exists. 
-                data can be a string, a buffer, an <AsyncIterable>, 
-                or an <Iterable> object.`;
-  await writeFile(filePath, fileData);
-  console.log("Write Success");
+  try {
+    let filePath ="C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\day9\package.json" ;
+    let fileData = await readFile(filePath, { encoding: "utf8" });
+    console.log(fileData);
+  } catch (e) {
+    console.log("Exception Occured :: ", e.message);
+  }
 }
 
 main();
