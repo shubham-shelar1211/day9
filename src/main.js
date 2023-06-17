57220826a46346508e43736742045ed53a96bcac
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:21:16 +0530

    async method

--- src/main.js
+++ src/main.js
@@ -1,12 +1,7 @@
-console.log("Hello 1");
-console.log("Hello 2");
-
-// is it blocking the next two stament ?
-// this is non blocking
-// non blocking helps to achive async prog
-setTimeout(() => {
+ async function hello() {
+    console.log("Hello 1");
+  }
+  
+  console.log("Hello 1");
+  hello();
   console.log("Hello 3");
-}, 0);
-
-console.log("Hello 4");
-console.log("Hello 5");
