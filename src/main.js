930033c8f49ae1dcade506e89223ed7d33f1a626
Shubham Shelar <shubhamshelar1211@gmail.com>
2023-06-17 09:14:32 +0530

    write a file

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-// How to read a file using NodeJS?
+// How to write a file using NodeJS?
 // this is a internal module.
-import { readFileSync } from "node:fs";
+import { readFileSync, writeFileSync } from "node:fs";
 
-let filePath = "C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\day9\package.json";
-let fileData = readFileSync(filePath, { encoding: "utf-8" });
-console.log(fileData);
+let filePath = "C:\Users\Shubham\Desktop\Cdac Kh WPT  SM Data\day9\output.txt";
+let data = `Similar to the above fs.readSync function, 
+            this version takes an optional options object. 
+            If no options object is specified, it will 
+            default with the above values.`;
+writeFileSync(filePath, data);
+console.log("Write Success!!");
